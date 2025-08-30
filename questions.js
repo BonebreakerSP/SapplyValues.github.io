f535b91b7ee1c6c77a71bfa43274ba47c53648ec
BonebreakerSP <t1337767@gmail.com>
2025-08-30 10:01:04 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -114,6 +114,6 @@ questions =  [
     {"id": 112, "question": "The government should punish educational institutions for promoting LGBT views on sexuality and gender.", "effects": {"auth": 3, "prog": -3, "right": 3}},
     {"id": 113, "question": "The government should suspend all laws and the constitution in response to a perceived threat to protect the country.", "effects": {"auth": 5}},
     {"id": 114, "question": "The government should suspend build a border wall to restrict illegal immigration into the country.", "effects": {"auth": 3, "prog": -3, "right": 3}},
-    {"id": 115, "question": "The government should ban pride marches.", "effects": {"auth": 5, "prog": -5, "right": 5}}
-    {"id": 116, "question": "The government should not adhere to and withdraw from climate change agreements.", "effects": {"auth": 5, "prog": -5, "right": 5}},
+    {"id": 115, "question": "The government should ban pride marches.", "effects": {"auth": 5, "prog": -5, "right": 5}},
+    {"id": 116, "question": "The government should not adhere to and withdraw from climate change agreements.", "effects": {"auth": 5, "prog": -5, "right": 5}}
 ];
